1130cbb642943d1343565d69d57399820a9e8447
MrSTARK28 <amandhok703@gmail.com>
2024-05-18 01:53:22 +0530

    First Commit

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../App.css';
+
+
+export default function Nav() {
+    return (
+            <div className="Parent">
+                <div className="Child">Home</div>
+                <div className="Child">Blog</div>
+                <div className="Child">Activities</div>
+                <div className="Child">People</div>
+                <div className="Child">Form</div>
+            </div>
+            );
+}
