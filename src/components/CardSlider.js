7226ab217a925a7ae00581ad37610a23f61eb2d0
MrSTARK28 <amandhok703@gmail.com>
2024-05-18 13:33:11 +0530

    started to make cardslider

---
+++ src/components/CardSlider.js
@@ -0,0 +1,54 @@
+import React from "react";
+import events from '../assets/events.webp';
+import './CardSlider.css';
+
+export default function CardSlider() {
+    const data = [
+        {
+            eventname: "Event Name",
+            img: 'events.jpeg',
+            info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo.",
+        },
+        {
+            eventname: "Event Name",
+            img: 'events.jpeg',
+            info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo.",
+        },
+        {
+            eventname: "Event Name",
+            img: 'events.jpeg',
+            info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo.",
+        },
+        {
+            eventname: "Event Name",
+            img: 'events.jpeg',
+            info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo.",
+        },
+        {
+            eventname: "Event Name",
+            img: 'events.jpeg',
+            info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo.",
+        },
+        {
+            eventname: "Event Name",
+            img: 'events.jpeg',
+            info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo.",
+        },
+    ]
+    return (
+        <div className="S1">
+            <div className="S2">
+                {data.map((d) =>(
+                    <div className="C1">
+                            <img src="d.img" alt=""/>
+                        <div>
+                            <p>{d.eventname}</p>
+                            <p>{d.info}</p>
+                            <button>Read More</button>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
